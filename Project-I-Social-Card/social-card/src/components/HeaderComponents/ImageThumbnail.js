296013d64b14de25c775bc87aa5e2a8ae2929641
Raidon Amakobe <Raidonmgmt@gmail.com>
2019-04-24 10:49:41 -0400

    Card Css and fixes

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './Header.css';
- 
+
 const ImageThumbnail = () => {
-    return (
-        <div className = "thumb-wrapper">
-        <img
-            alt = "lambda logo"
-            className = "thumb-img"
-            src = "https://ibin.co/3whrpkSBbz81.png"/>
-        </div>
-    );
+  return (
+    <div className="thumb-wrapper">
+      <img
+        alt="lambda logo"
+        className="thumb-img"
+        src="https://ibin.co/3whrpKSBbZ81.png"
+      />
+    </div>
+  );
 };
 
 export default ImageThumbnail;
